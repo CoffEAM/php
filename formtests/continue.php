2c7664782de015bd6b4c34b87f46514474553cb0
lenya_official <amlen.ya@mail.ru>
2025-11-29 13:41:03 +0200

    Изменение файла continue.php. Закрытие сессии после обновления страницы

--- formtests/continue.php
+++ formtests/continue.php
@@ -4,6 +4,14 @@ if (isset($_SESSION['forename'])) {
     $forename = $_SESSION['forename'];
     $surname = $_SESSION['surname'];
 
+    destroy_session_and_data();
+
     echo "С возвращением, $forename.<br> Ваше полное имя $forename $surname.<br>";
 }
 else echo "Пожалуйста, для входа <a href='authenticate2.php'>щелкните здесь</a>";
+
+function destroy_session_and_data(){
+    $_SESSION = array();
+    setcookie(session_name(), '', time() - 2592000, '/');
+    session_destroy();
+}
